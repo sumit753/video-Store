9938f99f267dbe436105af1b4c4d6cff698e8417
sumit753 <sumit753@gmail.com>
2017-03-20 05:28:13 +0530

    exercise 1 Done

---
+++ WeBStore/Controllers/CustomerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeBStore.Models;
+
+namespace WeBStore.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+
+        public ActionResult Details(int Id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == Id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>()
+                            {
+                               new Customer {Id=1,Name="Sumit" },
+                               new Customer { Id=2,Name="Amit"},
+                               new Customer {Id=3,Name="James" },
+                               new Customer {Id=4,Name="Mohan" },
+                               new Customer {Id=5,Name="Tika" },
+                                new Customer {Id=6,Name="Saxena" },
+                                new Customer {Id=7,Name="Vibhuti" }
+
+                                 };
+        }
+    }
+}
--- WeBStore/Controllers/MoviesController.cs
+++ WeBStore/Controllers/MoviesController.cs
@@ -2,34 +2,60 @@
 using System.Web.Mvc;
 using WeBStore.Models;
 using WeBStore.ViewModel;
+using System.Linq;
 namespace WeBStore.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
+        [Route("movies")]
         public ActionResult Index()
         {
-            return View();
+            var Movies = GetMovies();
+            return View(Movies);
         }
 
-            [Route("movies/random")]
-    
-        public ActionResult Random()
+        public ActionResult Details(int Id)
         {
-            var movie = new Movie() { Id =1,Name="Shrek" };
-            var customers = new List<Customer>()
-                            {
-                               new Customer {Id=1,Name="Sumit" },
-                               new Customer { Id=2,Name="Amit"}
-                               
-                            };
+            var movie = GetMovies().SingleOrDefault(m => m.Id == Id);
 
-            var ViewModel = new CustomerMovie()
-            {
-                Movie = movie,
-                CustomerList = customers
-            };
-            return View(ViewModel);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+
+        }
+
+       
+        //following code was to test ViewModel demo
+
+       // [Route("movies/random")]
+       //public ActionResult Random()
+       // {
+       //     var movie = new Movie() { Id =1,Name="Shrek" };
+       //     var customers = 
+
+       //     var ViewModel = new CustomerMovie()
+       //     {
+       //         Movie = movie,
+       //         CustomerList = customers
+       //     };
+       //     return View(ViewModel);
+       // }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>()
+                            {
+                               new Movie {Id=1,Name="AgniPath" },
+                               new Movie { Id=2,Name="Shagird"},
+                               new Movie {Id=3,Name="Lagaan" },
+                               new Movie {Id=4,Name="Shrik" },
+                               new Movie {Id=5,Name="KudvenkatSeries" },
+                                new Movie {Id=6,Name="Algorithm" },
+                                new Movie {Id=7,Name="Sarforsh" }
+
+                                 };
         }
     }
 }
